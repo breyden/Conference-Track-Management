ff0fb1155b0d921c36e391a60676ec2901458257
MonyemorathoBreyden <mnybre002@myuct.ac.za>
2019-05-27 23:59:49 +0200

    implemented step 4-7 to complete the scheduling of talks into tracks

--- Conference.java
+++ Conference.java
@@ -143,7 +143,125 @@ public   Conference()
 
 
 
-  
+    public int ScheduleTalksIntoTracks(int trackCountIndex, List<Talks> trackTalks, int trackCount,int startTalkIndex , int totalTalkCount)
+    {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm a");
+        Calendar cal = new GregorianCalendar();
+        cal.set(Calendar.HOUR, 9);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.AM_PM, Calendar.AM);
+
+        int sum180 = Configuration.MORNING_MINUTES;
+        int sum240 = Configuration.AFTERNOON_MINUTES;
+
+        int TalkIndex;
+
+        String sessionTime;
+        String SessionTitle;
+
+        for(TalkIndex=startTalkIndex; TalkIndex< totalTalkCount;TalkIndex++) {
+
+
+            // Get the combination of 180 and fill it
+            if (sum180 >= trackTalks.get(TalkIndex).getMinutes()) {
+                sum180 = sum180 - trackTalks.get(TalkIndex).getMinutes();
+                sessionTime = sdf.format(cal.getTime()) + " " + trackTalks.get(TalkIndex).getTitle() + " " + trackTalks.get(TalkIndex).getMinutes() + "min";
+                trackTalks.get(TalkIndex).setTitle(sessionTime);
+                cal.add(Calendar.MINUTE, trackTalks.get(TalkIndex).getMinutes());
+                SessionTitle = "Track" + " " + (trackCountIndex + 1);
+                trackTalks.get(TalkIndex).setTrackTitle(SessionTitle);
+            }
+            if (sum180 < trackTalks.get(TalkIndex).getMinutes())
+                break;
+
+            if (sum180 > 0)
+                continue;
+
+            if (sum180 <= 0)
+                break;
+        }
+
+        trackTalks.get(TalkIndex).setLunchFlag(true);
+        sessionTime = "12:00 PM" + " " + "Lunch";
+        trackTalks.get(TalkIndex).setLunchTitle(sessionTime);
+        cal.add(Calendar.MINUTE, 60);
+
+        TalkIndex++;
+
+        for(;TalkIndex< totalTalkCount;TalkIndex++) {
+            // Get the combination of 180 
+            if (sum240 >= trackTalks.get(TalkIndex).getMinutes()) {
+                sum240 = sum240 - trackTalks.get(TalkIndex).getMinutes();
+                sessionTime = sdf.format(cal.getTime()) + " " + trackTalks.get(TalkIndex).getTitle() + " " + trackTalks.get(TalkIndex).getMinutes() + "min";
+                trackTalks.get(TalkIndex).setTitle(sessionTime);
+                cal.add(Calendar.MINUTE, trackTalks.get(TalkIndex).getMinutes());
+                SessionTitle = "Track" + " " + (trackCountIndex + 1);
+                trackTalks.get(TalkIndex).setTrackTitle(SessionTitle);
+            }
+            if (sum240 < trackTalks.get(TalkIndex).getMinutes())
+                break;
+
+            if (sum240 > 0)
+                continue;
+
+            if (sum240 <= 0)
+                break;
+        }
+
+        if(totalTalkCount == (TalkIndex))
+            --TalkIndex;
+        trackTalks.get(TalkIndex).setNetworkingFlag(true);
+        sessionTime = "5:00 PM" + " " + "Networking Event";
+        trackTalks.get(TalkIndex).setNetworkingTitle(sessionTime);
+
+        TalkIndex++;
+        return TalkIndex;
+
+    }
+
+
+
+    /*
+     * Print out the Talks from  trackTalks,
+    */
+    public void OutputOfTalksIntoTracks(List<Talks> trackTalks){
+
+        System.out.println("Test Output :");
+        System.out.println("");
+        String TrackTitle = "dummyValue";
+
+        // Output the talks into tracks based on the totalTalks and the count of Talks.
+        for(int trackCountIndex=0;trackCountIndex<trackTalks.size();trackCountIndex++)
+        {
+
+            // Print the Track Title
+            if(!TrackTitle.equals(trackTalks.get(trackCountIndex).getTrackTitle()))
+            {
+                System.out.println(trackTalks.get(trackCountIndex).getTrackTitle() + ":");
+                System.out.println("");
+                TrackTitle = trackTalks.get(trackCountIndex).getTrackTitle();
+            }
+
+            // Print the prepared talk's title for this Track
+            System.out.println(trackTalks.get(trackCountIndex).getTitle());
+
+            // output the Lunch Title
+            if(trackTalks.get(trackCountIndex).isLunchFlag())
+            {
+                System.out.println(trackTalks.get(trackCountIndex).getLunchTitle());
+            }
+
+            // output the Networking Title
+            if(trackTalks.get(trackCountIndex).isNetworkingFlag())
+            {
+                System.out.println(trackTalks.get(trackCountIndex).getNetworkingTitle());
+                System.out.println("");
+                System.out.println("");
+            }
+
+        }
+    }
   
 
 
