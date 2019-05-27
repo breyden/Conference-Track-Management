9bd9fb1e67d0f697e1f8dae4c9e6ee5a4a71bcdc
MonyemorathoBreyden <mnybre002@myuct.ac.za>
2019-05-27 20:28:39 +0200

    allows passing the configuration parameters withou having to intantiate a Conference object

---
+++ Configuration.java
@@ -0,0 +1,13 @@
+public class Configuration
+{
+
+   //morning session total time in minutes.
+    public static final int MORNING_MINUTES = 180;
+    //afternoon session total time in minutes.
+    public static final int AFTERNOON_MINUTES = 240;
+    //total time in minutes.
+    public static final Double TOTAL_TRACK_MINUTES = 420.0;
+    // Read data from file and create a list of String.
+    public static final String TALKS_INPUT_FILE = "/Users/breydenmonyemoratho/Desktop/PRIVY SEAL SOLUTION/TalksInput.txt";
+
+}
